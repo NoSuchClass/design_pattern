fd27219e1eca80578c5e22ff2f05490c4e2c5205
NoSuchClass <a343788619@163.com>
2019-06-04 20:04:16 +0800

    类适配模式

---
+++ src/structural/adapter/classadapter/ConcreteTarget.java
@@ -0,0 +1,8 @@
+package structural.adapter.classadapter;
+
+public class ConcreteTarget implements Target {
+    @Override
+    public void request() {
+        System.out.println("concreteTarget目标方法");
+    }
+}
